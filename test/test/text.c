4a17aa23889576c85c6710bd7b322e7100824807
徐洋洋 <qiuyuxinchen@example.com>
2022-07-15 15:15:41 +0800

    测试test

---
+++ test/test/text.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+//void test1(int arr[])
+//{
+//	int len1 = sizeof(arr);
+//	printf("%d\n", len1);
+//	//printf("%d\n", len2);
+//}
+//void test2(int* arr)
+//{
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(arr[0]));
+//	int len = sizeof(arr) / sizeof(arr[0]);
+//	printf("%d\n", len);
+//
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5 };
+//	test1(arr);
+//	test2(&arr);
+//	printf("%d",sizeof(arr));
+//	return 0;
+//}
+
+//struct s
+//{
+//	char c;
+//	double m;
+//	int t;
+//
+//};
+//int main()
+//{
+//
+//	printf("%d", sizeof(struct s));
+//	return 0;
+//}
+int main()
+{
+	printf("I lost my cellphone!");
+
+	return 0;
+}
