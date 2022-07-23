b728a6ca19d8ec371b9a5d19061723c4789772ea
徐洋洋 <1952487538@qq.com>
2022-07-23 15:56:39 +0800

    C语言练习

---
+++ 2022.7.23/2022.7.23/test.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include<stdio.h>
+#define M 10
+enum color
+{
+	red,
+	blue = 5,
+	yellow
+};
+int main()
+{
+
+	enum color t = red;
+	//red = 2;
+	//int arr[blue] = { 0 };
+	//int arr1[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	//int arr2[10];
+	//int arr3[] = { 1,2,3,4,5,6,7,8,9,10 };
+	//int sz = sizeof(arr3) / sizeof(arr3[0]);
+	//printf("%d", sz);
+	//int arr4[10] = { 0 };
+	//int arr5[10] = { 1,2,3,4,5 };
+	//int arr[10] = { 0 };
+	//int sz = sizeof(arr) / sizeof(arr[0]);
+	//for (int i = 0; i < sz; i++)
+	//{
+	//	arr[i] = 1;
+	//	printf("%d\n", arr[i]);
+	//}
+	//const int x = 10;
+	//int arr[x];
+	/*int arr[M] = { 0 };*/
+
+
+	return 0;
+}
